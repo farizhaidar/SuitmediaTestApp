77251d177cfa053dd1ffbd537ac31a5514b70d63
farizhaidar <farizhaidar00@gmail.com>
2025-07-18 03:37:50 +0700

    upload profil

--- app/src/main/java/com/farizz/suitmediaapp/pages/PalindromeActivity.kt
+++ app/src/main/java/com/farizz/suitmediaapp/pages/PalindromeActivity.kt
@@ -1,9 +1,15 @@
 package com.farizz.suitmediaapp.pages
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.farizz.suitmediaapp.databinding.ActivityPalindromeBinding
 
 class PalindromeActivity : AppCompatActivity() {
@@ -15,11 +21,17 @@ class PalindromeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupListeners()
+        binding.ivProfile.setOnClickListener {
+            imagePickerLauncher.launch("image/*")
+        }
+
     }
 
     private fun setupListeners() {
         binding.btnCheck.setOnClickListener { handlePalindromeCheck() }
         binding.btnNext.setOnClickListener { handleNextButton() }
+        binding.ivProfile.setOnClickListener { pickImageFromGallery() }
+
     }
 
     private fun handlePalindromeCheck() {
@@ -53,4 +65,32 @@ class PalindromeActivity : AppCompatActivity() {
         intent.putExtra("EXTRA_NAME", name)
         startActivity(intent)
     }
+
+    private val pickImageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val imageUri: Uri? = result.data?.data
+                if (imageUri != null) {
+                    binding.ivProfile.setImageURI(imageUri)
+                } else {
+                    Toast.makeText(this, "Gagal memilih gambar", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        pickImageLauncher.launch(intent)
+    }
+
+    private val imagePickerLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        uri?.let {
+            Glide.with(this)
+                .load(it)
+                .circleCrop()
+                .into(binding.ivProfile)
+        }
+    }
+
 }
